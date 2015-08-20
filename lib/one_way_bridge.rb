4d4db33e0a52bc1325cef5484ac668d393bd7928
Yasuhito Takamiya <yasuhito@gmail.com>
2015-08-20 09:51:49 +0900

    コントローラとGemfileを追加.

---
+++ lib/one_way_bridge.rb
@@ -0,0 +1,46 @@
+class OneWayBridge < Trema::Controller
+  def packet_in(datapath_id, message)
+    out_port = { 1 => 2, 2 => 1 }.fetch(message.in_port)
+    add_flow datapath_id, message.source_mac, message.in_port, out_port
+    send_packet datapath_id, message, out_port
+    add_drop_flow datapath_id, message.source_mac, out_port
+  end
+
+  def flow_removed(datapath_id, messsage)
+    delete_flow datapath_id, message.match.ether_source_address
+  end
+
+  private
+
+  def add_flow(datapath_id, source_mac, in_port, out_port)
+    send_flow_mod_add(
+      datapath_id,
+      idle_timeout: 10 * 60,
+      match: Match.new(in_port: in_port, ether_source_address: source_mac),
+      actions: SendOutPort.new(out_port)
+    )
+  end
+
+  def add_drop_flow(datapath_id, source_mac, in_port)
+    send_flow_mod_add(
+      datapath_id,
+      idle_timeout: 10 * 60,
+      match: Match.new(in_port: in_port, ether_source_address: source_mac)
+    )
+  end
+
+  def send_packet(datapath_id, message, out_port)
+    send_packet_out(
+      datapath_id,
+      packet_in: message,
+      actions: SendOutPort.new(out_port)
+    )
+  end
+
+  def delete_flow(datapath_id, source_mac)
+    send_flow_mod_delete(
+      datapath_id,
+      match: Match.new(ether_source_address: source_mac)
+    )
+  end
+end
